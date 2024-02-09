653f5ebf8e6238328de19589b5694f9c0f589e71
Ethan0754 <EthanPendergraft@gmail.com>
2024-02-09 15:08:20 -0600

    Added ball movement
    everything besides lose condition and edge detection

--- untitled1.py
+++ untitled1.py
@@ -30,16 +30,41 @@ def upScreen(aDirection, bDirection, aLine, bLine, ball, bMoveX, bMoveY, bX, bY,
     bX += bMoveX
     bY += bMoveY
     if(detectPaddle(bX, bY, paddleAY, paddleBY) == "Left"):
-        bMoveX, bMoveY = ballDirection(bMoveX, bMoveY, "Left")
+        bMoveX, bMoveY = ballDirection(bMoveX, bMoveY, "Left", paddleAY, paddleBY, bX, bY)
     if(detectPaddle(bX, bY, paddleAY, paddleBY) == "Right"):
-        bMoveX, bMoveY = ballDirection(bMoveX, bMoveY, "Right")
+        bMoveX, bMoveY = ballDirection(bMoveX, bMoveY, "Right", paddleAY, paddleBY, bX, bY)
     return bMoveX, bMoveY, bX, bY, paddleAY, paddleBY
 
     
-def ballDirection(bMoveX, bMoveY, side):
+def ballDirection(bMoveX, bMoveY, side, paddleAY, paddleBY, bX, bY):
     bMoveX = -bMoveX
-    bMoveY = bMoveY
+    if(bY > paddleAY-10 and bY < paddleAY+10):
+        bMoveY = bMoveY
+    elif(bY > paddleAY+10 and bY < paddleAY+20):
+        bMoveY = 1
+    elif(bY > paddleAY+20 and bY < paddleAY+30):
+        bMoveY = 2
+    elif(bY > paddleAY+30 and bY < paddleAY+40):
+        bMoveY = 3
+    elif(bY > paddleAY+40 and bY < paddleAY+50):
+        bMoveY = 4
+    elif(bY > paddleAY-10 and bY < paddleAY-20):
+        bMoveY = -1
+    elif(bY > paddleAY-20 and bY < paddleAY-30):
+        bMoveY = -2
+    elif(bY > paddleAY-30 and bY < paddleAY-40):
+        bMoveY = -3
+    elif(bY > paddleAY-40 and bY < paddleAY-50):
+        bMoveY = -4
+    if(bX == 0 or bX == 1000):
+        bMoveY = -bMoveY
+
     return bMoveX,bMoveY
+
+
+def detectEdge(bY):
+    if(bY == 0 or bY == 1000):
+        return true
         
 def detectPaddle(bX, bY, paddleAY, paddleBY):
     print(bX)
